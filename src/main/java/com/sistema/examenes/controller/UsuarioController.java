a334ad219dfe117ee1951b7efcd36b348dac2c83
MauricioOcampo <maoocampo@hotmail.com>
2023-05-05 11:14:44 -0500

    Carga del proyecto sistema examenes backend

--- src/main/java/com/sistema/examenes/controller/UsuarioController.java
+++ src/main/java/com/sistema/examenes/controller/UsuarioController.java
@@ -1,9 +1,18 @@
 package com.sistema.examenes.controller;
 
+import com.sistema.examenes.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Para acceder a esta apiRest debemos colocamos /usuarios
 @RestController
-@RequestMapping
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    //@PostMapping
 }
